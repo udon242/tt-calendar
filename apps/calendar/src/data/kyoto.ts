e6f340ed25c458e68ef468411c4a8be48086748d
udon <ylu242@gmail.com>
2022-09-25 08:13:55 +0900

    Update kyoto.ts

--- apps/calendar/src/data/kyoto.ts
+++ apps/calendar/src/data/kyoto.ts
@@ -14,7 +14,7 @@ export const schedules: Schedule[] = [
   },
   {
     start: '202209241200',
-    title: 'KA神奈川 - 京都',
+    title: 'KA神奈川 4-0 京都',
     location: 'アリーナ立川立飛',
   },
   { start: '202210081600', title: '九州 - 京都', location: 'アクシオン福岡' },
