5324f58997a35019e1d50bfea9bd21e7c7b5bf48
udon242 <ylu242@gmail.com>
2022-10-05 08:48:53 +0900

    clean: データをまとめる

--- packages/schema/schedule.ts
+++ packages/schema/schedule.ts
@@ -1,5 +1,21 @@
+export type Team =
+  | 'tt-saitama'
+  | 'km-tokyo'
+  | 'okayama'
+  | 'ryukyu'
+  | 'ka-kanagawa'
+  | 'top-nagoya'
+  | 'kyoto'
+  | 'red-elf'
+  | 'np-mallets'
+  | 'kyusyu';
+
 export type Schedule = {
   title: string;
   start: string;
   location: string;
+  homeTeam?: Team;
+  awayTeam?: Team;
+  homeScore?: 0 | 1 | 2 | 3 | 4;
+  awayScore?: 0 | 1 | 2 | 3 | 4;
 };
