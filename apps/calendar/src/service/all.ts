14b82cd21f9e2734697b00d38d4f992dd6ff4176
udon242 <ylu242@gmail.com>
2022-11-18 10:18:25 +0900

    clean: eslint, prettier追加

--- apps/calendar/src/service/all.ts
+++ apps/calendar/src/service/all.ts
@@ -5,6 +5,6 @@ import scheduleAdapter from '../adapter/scheduleAdapter';
 
 const CALENDAR_NAME = 'Tリーグ 日程';
 const { value } = createEvents(
-    scheduleAdapter.schedulesToEvents(CALENDAR_NAME, allSchedules)
+  scheduleAdapter.schedulesToEvents(CALENDAR_NAME, allSchedules)
 );
 export const getAllSchedule = () => value;
