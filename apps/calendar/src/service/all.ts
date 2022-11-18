2fe8ed46603abd4d94a014cd6dfe501c3f923a21
udon242 <ylu242@gmail.com>
2022-11-18 10:05:24 +0900

    feat: allエンドポイントを追加

---
+++ apps/calendar/src/service/all.ts
@@ -0,0 +1,10 @@
+import { createEvents } from 'ics';
+
+import { allSchedules } from '@tt-calendar/data/all';
+import scheduleAdapter from '../adapter/scheduleAdapter';
+
+const CALENDAR_NAME = 'Tリーグ 日程';
+const { value } = createEvents(
+    scheduleAdapter.schedulesToEvents(CALENDAR_NAME, allSchedules)
+);
+export const getAllSchedule = () => value;
